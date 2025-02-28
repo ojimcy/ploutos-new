140b13d0335fdbd7f757031b26c70143b75016a0
ojimcy <ojimcy247@gmail.com>
2025-02-28 05:21:06 +0100

    localisation

---
+++ translations/translations.js
@@ -0,0 +1,269 @@
+const translations = {
+  en: {
+    siteTitle:
+      'Ploutos: Revolutionizing the use of Cryptocurrency in the world',
+    home: 'Home',
+    about: 'About',
+    faq: 'FAQ',
+    airdrop: 'Airdrop',
+    trade: 'Trade',
+    whitepaper: 'Whitepaper',
+    language: 'Language',
+    english: 'English',
+    french: 'French',
+    mainnet: 'Mainnet',
+    testnet: 'Testnet',
+    homeTitle: 'Redefining Cryptocurrency.',
+    homeSubTitle:
+      'With a limited supply of tokens released gradually over eight (8) years, $PLTL is set to deliver a stable, secure and usable Cryptocurrency. Join us as we change the narrative and set the stage for a stable and rewarding crypto experience!',
+    buy: 'Buy $PLTL Now',
+    audit: 'Audit',
+    aboutTitle: 'About Ploutos',
+    aboutSubTitle:
+      '**Revolutionizing Digital Finance** PLOUTOS is more than just a cryptocurrency; it is a visionary project aimed at reshaping the future of digital finance. Built on innovation, transparency, and utility, $PLTL is setting new standards for global adoption.',
+    corePrinciples: 'Core Principles',
+    unmatchedScalability: 'Unmatched Scalability',
+    realWorldApplication: 'Real-World Application',
+    corePrinciplesSubTitle:
+      'Ploutos is founded on three pillars: **innovation, transparency, and utility**, addressing gaps in traditional cryptocurrencies.',
+    unmatchedScalabilitySubTitle:
+      'Built on the **BASE network**, Ploutos ensures unmatched processing speeds and scalability for high transaction volumes.',
+    realWorldApplicationSubTitle:
+      'Ploutos is designed to be a practical and useful cryptocurrency, providing real-world applications and solutions for everyday use.',
+    realWorldApplication: 'Real-World Application',
+    realWorldApplicationSubTitle:
+      'Ploutos aims to drive **widespread adoption** with practical use cases, setting a new standard for usability in digital finance.',
+    readMore: 'Read More',
+    kyc: 'KYC',
+    features: 'Features',
+    featuresTitle: 'CORE FEATURES',
+    featuresSubTitle:
+      'Discover some of the powerful features that set $PLTL apart and makes it the game changer.',
+    enhancedSecurity: 'Enhanced Security on ALL Platforms',
+    enhancedSecuritySubTitle:
+      'The PLOUTOS project is based on integrity and trust. We prioritize the safety of our users. Our smart contract underwent a rigorous auditing by Industry leaders (Cyberscope.io) to identify and mitigate potential vulnerabilities, thereby ensuring maximum security for our users on the platform.',
+    nftDevelopment: 'NFT Development and Partnerships',
+    nftDevelopmentSubTitle:
+      'Collaborating with established NFT ecosystems to introduce unique digital assets, fostering creativity and ownership in the digital space. Partnering with these platforms can foster a vibrant digital art and collectible marketplace.',
+    ploutosPay: 'PLOUTOS Pay with Telegram App & Smart Wallet',
+    ploutosPaySubTitle:
+      'This feature enables an easier user experience when it comes to seamless crypto transactions. By integrating the CoinBase smart wallet on Telegram, we have successfully brought cryptocurrency to users at their most comfortable locations i.e on their chat spaces. This integration also enhances accessibility and usability of $PLTL as well as other tokens on the BASE network.',
+    communityEngagement: 'Community Engagement',
+    communityEngagementSubTitle:
+      'Our community is very important to us. This is why we prioritize community engagement and sustainability through various programs, partnerships, contributing to a greener future, and Airdrop with the aim of empowering our community.',
+    ecosystem: 'Ecosystem',
+    ecosystemSubTitle:
+      'Explore the revolutionary services offered within the PloutosLabs ecosystem.',
+    buyAndSell: 'Buy and Sell',
+    buyAndSellSubTitle: 'Trade digital assets seamlessly.',
+    learnMore: 'Learn More...',
+    swap: 'Swap',
+    swapSubTitle: 'Exchange cryptocurrencies effortlessly.',
+    wallet: 'Wallet',
+    walletSubTitle: 'Save and store assets securely on the Base Network.',
+    recharge: 'Recharge',
+    rechargeSubTitle:
+      'Purchase data, pay electricity bills, and TV subscriptions',
+    ecommerce: 'E-commerce',
+    ecommerceSubTitle: 'Shop online with ease (coming soon).',
+    workspace: 'Decentralized Workspace',
+    workspaceSubTitle: 'Platform for online remote work (coming soon).',
+    currentMarket: 'Current Market',
+    marketChange: 'Market change in the last 24 hours',
+    marketChangeValue: '+0.00%',
+    allTokens: 'All Tokens',
+    price: 'Price',
+    totalVolume: 'Total Volume',
+    oneHour: '1H',
+    twentyFourHours: '24H',
+    sevenDays: '7D',
+    faqs: 'FAQS',
+    frequentlyAskedQuestions: 'Frequently asked Question',
+    frequentlyAskedQuestionsSubTitle:
+      'Find answers to some of the most commonly asked questions about our services and operations.',
+    whatIsPloutos: 'What is Ploutos?',
+    whatIsPloutosAnswer:
+      'Ploutos is a utility token built on the BASE network. The BASE network is a Layer 2 EVM network.',
+    howCanIBuyPloutos: 'How can I Buy Ploutos?',
+    howCanIBuyPloutosAnswer:
+      'You can BUY Ploutos tokens in the presale stage right now on our Telegram bot.',
+    whatWillPloutosBeUsedFor: 'What will Ploutos be used for?',
+    whatWillPloutosBeUsedForAnswer:
+      'Ploutos will be used for e-commerce purchases, as a means of exchange, and a store of value. The Ploutoslabs team is focused on development that will bring real value to the Ploutos token and expanding the use of cryptocurrency at large.',
+    futureDevelopments:
+      'What future developments do we expect from Ploutoslabs?',
+    futureDevelopmentsAnswer:
+      'In the immediate future, the Ploutoslabs team will finish work on its e-commerce store, enabling users to buy various products and pay directly in cryptocurrency, including Ploutos tokens at launch. Users will also be able to pay for utilities such as electricity bills and cable subscriptions with partner cryptocurrencies via our Telegram wallet, bringing real use cases to the crypto space and the Ploutos token. Much more is to come.',
+    smartContractAudited: 'Is the Ploutos Smart contract Audited?',
+    smartContractAuditedAnswer:
+      'Yes, the Ploutos Smart Contract is fully audited by Cyberscope.io. You can find the Audit report here.',
+    teamDoxxed: 'Is the Ploutoslabs team DOXXED?',
+    teamDoxxedAnswer:
+      'No, the Ploutoslabs team is not DOXXED. However, the team has completed KYC with Cyberscope.io.',
+    utilitiesBuilt: 'What utilities has the Ploutoslabs team already built?',
+    utilitiesBuiltAnswer:
+      'Right now, you can enjoy the beautiful play-to-earn game on the Ploutoslabs Telegram bot. You can BUY and SELL cryptocurrencies directly with more than 18 fiat currencies, swap more than 300 different cryptocurrencies on various networks, all through the Ploutos wallet in the Telegram bot.',
+    footerContent:
+      'Ploutos Labs is dedicated to providing innovative blockchain solutions with a focus on security, usability, and community engagement. Join us on this revolutionary journey.',
+    followUs: 'Follow Us',
+    marketplace: 'Marketplace',
+    tokenomics: 'Tokenomics',
+    support: 'Support',
+    announcement: 'Announcement',
+    mediaKit: 'Media Kit',
+    company: 'Company',
+    copyright: 'Copyright © Ploutoslabs all rights reserved.',
+    termsCondition: 'Terms Condition',
+    privacyPolicy: 'Privacy Policy',
+    tradeTitle: 'Trade on Ploutos',
+    // meta tags
+    description:
+      'Join Ploutos on the BASE Network to experience unmatched scalability and real-world cryptocurrency applications. Secure, innovative, and user-friendly—discover how Ploutos is redefining digital finance.',
+    keywords:
+      'Ploutos, cryptocurrency, blockchain, digital finance, future of finance',
+    ogDescription: 'Revolutionizing the use of Cryptocurrency in the world',
+    tradeMetaDescription:
+      'Trade digital assets seamlessly on Ploutos. Buy, sell, and swap cryptocurrencies with ease.',
+  },
+  fr: {
+    siteTitle:
+      "Ploutos: Révolutionnant l'utilisation de la Cryptomonnaie dans le monde",
+    home: 'Accueil',
+    about: 'À propos',
+    faq: 'FAQ',
+    airdrop: 'Airdrop',
+    trade: 'Commerce',
+    whitepaper: 'Livre blanc',
+    language: 'Langue',
+    english: 'Anglais',
+    french: 'Français',
+    mainnet: 'Mainnet',
+    testnet: 'Testnet',
+    homeTitle: 'Redéfinir le Cryptomonnaie.',
+    homeSubTitle:
+      'Avec une offre limitée de jetons libérés progressivement sur huit (8) ans, $PLTL est conçu pour offrir une monnaie stable, sécurisée et utilisable. Rejoignez-nous pour changer la narration et établir le ton pour une expérience crypto stable et récompensante!',
+    buy: 'Acheter $PLTL',
+    audit: 'Audit',
+    aboutTitle: 'À propos de Ploutos',
+    aboutSubTitle:
+      "**Révolutionnant le Financement Numérique** PLOUTOS est plus qu'une simple cryptomonnaie; c'est un projet visionnaire visant à révolutionner le financement numérique futur. Construit sur l'innovation, la transparence et l'utilité, $PLTL établit de nouveaux standards pour l'adoption globale.",
+    corePrinciples: 'Principes Fondamentaux',
+    unmatchedScalability: 'Scalabilité Sans Précédent',
+    realWorldApplication: 'Application dans le Monde Réel',
+    corePrinciplesSubTitle:
+      'Ploutos est fondé sur trois piliers: **innovation, transparence, et utilité**, abordant les lacunes des cryptomonnaies traditionnelles.',
+    unmatchedScalabilitySubTitle:
+      'Construit sur le réseau **BASE**, Ploutos garantit une scalabilité sans précédent et une capacité de traitement sans égal pour les volumes de transactions élevés.',
+    realWorldApplicationSubTitle:
+      'Ploutos est conçu pour être une cryptomonnaie pratique et utile, offrant des applications et solutions pratiques pour une utilisation quotidienne.',
+    realWorldApplication: 'Application dans le Monde Réel',
+    realWorldApplicationSubTitle:
+      "Ploutos vise à encourager une adoption **large et durable** avec des cas d'utilisation pratiques, établissant de nouveaux standards pour l'utilisabilité dans le financement numérique.",
+    readMore: 'Lire Plus',
+    kyc: 'KYC',
+    features: 'Fonctionnalités',
+    featuresTitle: 'FONCTIONNALITÉS ESSENTIELLES',
+    featuresSubTitle:
+      'Découvrez certaines des fonctionnalités puissantes qui distinguent $PLTL et le rendent le changeur de jeu.',
+    enhancedSecurity: 'Sécurité Renforcée sur Toutes les Plateformes',
+    enhancedSecuritySubTitle:
+      "Le projet PLOUTOS est basé sur l'intégrité et la confiance. Nous privilégions la sécurité de nos utilisateurs. Notre contrat intelligent a été soumis à une évaluation rigoureuse par des leaders de l'industrie (Cyberscope.io) pour identifier et atténuer les vulnérabilités potentielles, ce qui garantit une sécurité maximale pour nos utilisateurs sur la plateforme.",
+    nftDevelopment: 'Développement NFT et Partenariats',
+    nftDevelopmentSubTitle:
+      "Collaboration avec des écosystèmes NFT établis pour introduire des actifs numériques uniques, stimulant la créativité et l'appropriation dans l'espace numérique. Partenariat avec ces plateformes peut favoriser une marketplace numérique vibrant et une collection de biens.",
+    ploutosPay: "PLOUTOS Pay avec l'application Telegram et le Wallet Smart",
+    ploutosPaySubTitle:
+      "Cette fonctionnalité permet une expérience utilisateur plus facile lorsqu'il s'agit de transactions crypto sans friction. En intégrant le wallet intelligent CoinBase sur Telegram, nous avons réussi à apporter la cryptomonnaie aux utilisateurs dans leurs emplacements les plus confortables, c'est-à-dire dans leurs espaces de conversation. Cette intégration renforce également l'accessibilité et l'utilisabilité de $PLTL ainsi que d'autres jetons sur le réseau BASE.",
+    communityEngagement: 'Engagement de la Communauté',
+    communityEngagementSubTitle:
+      "Notre communauté est très importante pour nous. C'est pourquoi nous privilégions l'engagement communautaire et la durabilité via divers programmes, partenariats, contribuant à un avenir plus vert, et Airdrop avec pour objectif de renforcer notre communauté et récompenser les premiers utilisateurs.",
+    ecosystem: 'Ecosystème',
+    ecosystemSubTitle:
+      "Explorez les services innovants offerts dans l'écosystème PloutosLabs.",
+    buyAndSell: 'Acheter et Vendre',
+    buyAndSellSubTitle: 'Tradez des actifs numériques sans friction.',
+    learnMore: 'En savoir plus...',
+    swap: 'Échanger',
+    swapSubTitle: 'Échangez des cryptomonnaies sans friction.',
+    wallet: 'Portefeuille',
+    walletSubTitle:
+      'Enregistrez et stockez des actifs de manière sécurisée sur le réseau BASE.',
+    recharge: 'Recharger',
+    rechargeSubTitle:
+      "Acheter des données, payer les factures d'électricité et de télévision",
+    ecommerce: 'E-commerce',
+    ecommerceSubTitle: 'Acheter en ligne avec facilité (bientôt disponible).',
+    workspace: 'Espace de Travail Décentralisé',
+    workspaceSubTitle:
+      'Plateforme pour le travail à distance en ligne (bientôt disponible).',
+    currentMarket: 'Marché Actuel',
+    marketChange: 'Changement de marché dans les 24 dernières heures',
+    marketChangeValue: '+0.00%',
+    allTokens: 'Tous les Tokens',
+    price: 'Prix',
+    totalVolume: 'Volume Total',
+    oneHour: '1H',
+    twentyFourHours: '24H',
+    sevenDays: '7D',
+    faqs: 'FAQS',
+    frequentlyAskedQuestions: 'Questions Fréquemment Posées',
+    frequentlyAskedQuestionsSubTitle:
+      'Trouvez des réponses aux questions fréquemment posées sur nos services et opérations.',
+    whatIsPloutos: "Qu'est-ce que Ploutos?",
+    whatIsPloutosAnswer:
+      'Ploutos est un jeton utilitaire construit sur le réseau BASE. Le réseau BASE est un réseau de couche 2 EVM.',
+    howCanIBuyPloutos: 'Comment puis-je acheter Ploutos?',
+    howCanIBuyPloutosAnswer:
+      'Vous pouvez acheter des jetons Ploutos dans la phase de pré-vente actuelle sur notre bot Telegram.',
+    whatWillPloutosBeUsedFor: 'Pour quoi Ploutos sera-t-il utilisé?',
+    whatWillPloutosBeUsedForAnswer:
+      "Ploutos sera utilisé pour les achats en ligne, comme moyen d'échange et comme réserve de valeur. Le team Ploutoslabs est concentré sur le développement qui apportera de la valeur réelle au jeton Ploutos et étendra l'utilisation de la cryptomonnaie à grande échelle.",
+    futureDevelopments:
+      'Quels développements futurs attendons-nous de Ploutoslabs?',
+    futureDevelopmentsAnswer:
+      "Dans le court terme, le team Ploutoslabs terminera son travail sur son magasin en ligne, permettant aux utilisateurs d'acheter divers produits et de payer directement en cryptomonnaie, y compris les jetons Ploutos au lancement. Les utilisateurs pourront également payer pour les services tels que les factures d'électricité et de télévision avec des cryptomonnaies partenaires via notre wallet Telegram, apportant des cas d'utilisation réels au monde de la crypto et au jeton Ploutos. Encore plus est à venir.",
+    smartContractAudited:
+      'Est-ce que le contrat intelligent Ploutos est audite?',
+    smartContractAuditedAnswer:
+      "Oui, le contrat intelligent Ploutos est entièrement audité par Cyberscope.io. Vous pouvez trouver le rapport d'audit ici.",
+    teamDoxxed: 'Est-ce que le team Ploutoslabs est DOXXED?',
+    teamDoxxedAnswer:
+      "Non, le team Ploutoslabs n'est pas DOXXED. Cependant, le team a complété le KYC avec Cyberscope.io.",
+    utilitiesBuilt:
+      'Quels utilités le team Ploutoslabs a-t-il déjà construites?',
+    utilitiesBuiltAnswer:
+      'En ce moment, vous pouvez profiter du jeu de type "jouer pour gagner" sur le bot Telegram de Ploutoslabs. Vous pouvez acheter et vendre des cryptomonnaies directement avec plus de 18 devises fiduciaires, échanger plus de 300 différentes cryptomonnaies sur divers réseaux, tout cela via le wallet Ploutos dans le bot Telegram.',
+    footerContent:
+      "Ploutos Labs est dédié à fournir des solutions blockchain innovantes avec un focus sur la sécurité, l'utilisabilité et l'engagement communautaire. Rejoignez-nous sur ce voyage révolutionnaire.",
+    followUs: 'Suivez-nous',
+    marketplace: 'Marché',
+    tokenomics: 'Tokenomics',
+    support: 'Support',
+    announcement: 'Annonce',
+    mediaKit: 'Kit de Médias',
+    company: 'Entreprise',
+    copyright: 'Copyright © Ploutoslabs tous droits réservés.',
+    termsCondition: "Conditions d'utilisation",
+    privacyPolicy: 'Politique de confidentialité',
+    tradeTitle: 'Trade on Ploutos',
+    // meta tags
+    description:
+      'Rejoignez Ploutos sur le réseau BASE pour expériencer une scalabilité sans précédent et des applications de cryptomonnaie dans le monde réel. Sécurisé, innovant et convivial, découvrez comment Ploutos révolutionne le financement numérique.',
+    keywords:
+      'Ploutos, cryptomonnaie, blockchain, finance numérique, avenir du financement',
+    ogDescription:
+      "Révolutionnant l'utilisation de la Cryptomonnaie dans le monde",
+    tradeMetaDescription:
+      'Trade digital assets seamlessly on Ploutos. Buy, sell, and swap cryptocurrencies with ease.',
+  },
+};
+
+function setLanguage(lang) {
+  document.querySelectorAll('[data-translate]').forEach((element) => {
+    const key = element.getAttribute('data-translate');
+    element.textContent = translations[lang][key];
+  });
+}
+
+// Set default language
+setLanguage('en');
